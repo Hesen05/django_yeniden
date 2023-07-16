56be0b36461a3bdcec41b7219bc24492983188d5
Hesen05 <hesenzadeh663@gmail.com>
2023-07-17 00:43:00 +0400

    version2

--- home/urls.py
+++ home/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from home.views import *
 
 urlpatterns = [
-    path('', index),
-    path('about/', about_us),
-    path('blog/', blog),
+    path('', index, name = 'index'),
+    path('about/', about_us, name = 'about'),
+    path('blog/', blog, name = blog),
     path('blogdetail/', blogdetail),
     path('404error/', error404),
     path('contact/', contactus),
